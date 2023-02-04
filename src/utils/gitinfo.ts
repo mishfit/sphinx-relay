cd700ea9fd344afb508bd5d00fed193fc22e00e3
Mish Ochu <mishochu@mishochu.com>
2023-02-04 06:45:58 -0700

    use spawnSync over execSync

--- src/utils/gitinfo.ts
+++ src/utils/gitinfo.ts
@@ -1,9 +1,16 @@
-import { execSync } from 'child_process'
+import { spawnSync } from 'child_process'
 import { sphinxLogger } from './logger'
 
 function git(command: string): string | void {
   try {
-    return execSync(`git ${command}`).toString().trim()
+    const output = spawnSync(`git ${command}`)
+    const errorText = output.stderr.toString().trim()
+
+    if (errorText) {
+      sphinxLogger.error(errorText)
+    } else {
+      return output.stdout.toString().trim()
+    }
   } catch (e) {
     sphinxLogger.error(
       'Error running a git command, probably not running in a git repository'
